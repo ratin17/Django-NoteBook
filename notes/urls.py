79b34541dc1106662d57008ccd53d7f5bf7a3228
Mehrab Rabbi Ratin <ratin.cse@gmail.com>
2023-12-06 21:44:49 +0600

    model and form addition

--- notes/urls.py
+++ notes/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('home/',views.index,name='index'),
     path('notes/',views.notes,name='notes'),
     path('note/<int:note_id>',views.note,name='note'),
+    path('new_note/',views.new_note,name='new_note'),
     
     
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Note,Entry
+from .forms import Noteform
 
 MyNotes=[{'id':1,'title':'First Note','entries':['Entry 1.1','Entry 1.2','Entry 1.3']},
          {'id':2,'title':'Second Note','entries':['Entry 2.1','Entry 2.2','Entry 2.3']},
@@ -11,24 +13,31 @@ def index(request):
 
 def notes(request):
     
-    notes=MyNotes
+    notes=Note.objects.order_by('-date_added')
     
     context={'notes':notes}
     
     return render(request,'notes/notes.html',context)
 
 def note(request,note_id):
+    note=Note.objects.get(id=note_id)
+    entries=note.entry_set.order_by('date_added')
     
-    for mynote in MyNotes:
-        if mynote['id']==note_id:
-            note=mynote
-    
-    entries=note['entries']
     context={'note':note,'entries':entries}
     
     return render(request,'notes/note.html',context)
     
     
-
+def new_note(request):
+    if request.method !='POST':
+        form=Noteform()
+    else:
+        form=Noteform(data=request.POST)
+        if form.is_valid():
+            new_note=form.save()
+            new_note.save()
+            return redirect('notes:notes')
+    context={'form':form}
+    return render(request,'notes/new_note.html',context)
 
     
